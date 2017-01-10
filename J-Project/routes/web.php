caad625a1581f8df2847d785f6392689dca81e08
U1457103 <U1457103@unimail.hud.ac.uk>
2017-01-10 21:37:41 +0000

    still changes to be made

--- J-Project/routes/web.php
+++ J-Project/routes/web.php
@@ -18,6 +18,12 @@ Route::get('users', 'UsersController@index' );
 
 Route::get('users/{user}', 'UsersController@show' );
 
+Route::get('/', 'PagesController@home' );
+
+
+Route::get('songs', 'SongsController@index' );
+
+Route::get('songs/{song}', 'SongsController@show' );
 
 
 Route::post('songs/{song}/notes', 'NotesController@store');
@@ -32,8 +38,9 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('songs', 'SongsController@index' );
 
+Route::get('songs/{song}', 'SongsController@show' );
 
-
-
+Route::get('song', 'PagesController@song' );
 
