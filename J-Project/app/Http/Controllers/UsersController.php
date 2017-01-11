bc46a03f8207bf5debdb718e9c8ee4a75d680d69
Mohammed Jhangir Dastgir <u1457103@hud.ac.uk>
2017-01-11 13:53:06 +0000

    Update UsersController.php

--- J-Project/app/Http/Controllers/UsersController.php
+++ J-Project/app/Http/Controllers/UsersController.php
@@ -12,13 +12,6 @@ class UsersController extends Controller
     public function index()
     {
       $users =  User::all();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
 
       return view ('users.index', compact('users'));
     }
@@ -29,26 +22,12 @@ class UsersController extends Controller
     {
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
       return view ('users.index', compact('users'));
     }
     public function show(User $user)
     {
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
       return view('users.show', compact('user'));
     }
 
