f7582bbb3c506e3aacabfec621bc50d87b8906d2
Mohammed Jhangir Dastgir <u1457103@hud.ac.uk>
2017-01-11 14:00:12 +0000

    Update show.blade.php

--- J-Project/resources/views/users/show.blade.php
+++ J-Project/resources/views/users/show.blade.php
@@ -5,41 +5,18 @@
   <h1>{{$user->name}}</h1>
 
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
 <ul class="list-group">
 @foreach ($user->details as $detail)
 
 <li class="list-group-item">
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
 
 <ul>
 @foreach ($user->details as $detail)
 
 <li>
 
-<<<<<<< HEAD
 
-
-=======
-<<<<<<< HEAD
-=======
-
-
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
   {{$detail->body}}
 </li>
 
