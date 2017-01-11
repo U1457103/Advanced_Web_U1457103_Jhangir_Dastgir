b795b29daedef8b1e560f29d7feac46ff49cf09b
Mohammed Jhangir Dastgir <u1457103@hud.ac.uk>
2017-01-11 13:57:36 +0000

    Update welcome.blade.php

--- J-Project/resources/views/welcome.blade.php
+++ J-Project/resources/views/welcome.blade.php
@@ -12,23 +12,9 @@
 alt="Music Banner">
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
 
 <h1>Hello and welcome to my songs website!</h1>
 
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> acd777ceafb2eb5d49e94cdba13ba8f04e28f41e
->>>>>>> 3041dd117b6002ce8704aadf7a959ae6a58c309e
 @stop
